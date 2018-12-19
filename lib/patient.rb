c854c012a80b38bd7158b706145fd519058f2800
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-12-19 11:41:22 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -10,12 +10,15 @@ class Patient
   end
   
   def new_appointment(doctor,date)
-    Appointment.new(self,doctor,date)
+    appointment = Appointment.new(self,doctor,date)
   end
     
   def self.all 
     @@all 
   end 
+  
+  def appointments 
+    Appointment.all 
   
   
     
